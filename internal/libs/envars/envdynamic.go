c4f6f9cb24910db0c59d073fbcbd22d63f586984
Magomed Zakaryaev <zkryaev.dev@yandex.ru>
2024-11-15 14:14:12 +0300

    Improved ENV fetching

--- internal/libs/envars/envdynamic.go
+++ internal/libs/envars/envdynamic.go
@@ -1,7 +1,6 @@
 package envars
 
 import (
-	"fmt"
 	"os"
 	"strings"
 )
@@ -9,15 +8,17 @@ import (
 // get path from home dir
 // example: /home/$USER/Timeline/$GIVEN_PATH
 func GetPath(envName string) string {
-	user := os.Getenv("USER")
-	givenPath := os.Getenv(envName)
-	base := strings.Builder{}
-	base.Grow(len("/home") + len(user) + len("/Timeline"))
-	base.WriteString("/home/")
-	base.WriteString(user)
-	base.WriteString("/Timeline")
-	if strings.Contains(givenPath, base.String()) {
-		return givenPath
+	pathToFile := os.Getenv(envName)
+	rootProjectDir, _ := os.Getwd()
+	path := strings.Builder{}
+	path.Grow(len(rootProjectDir) + len(pathToFile))
+	path.WriteString(rootProjectDir)
+	if strings.Contains(pathToFile, path.String()) {
+		return pathToFile
 	}
-	return fmt.Sprintf("%s/%s", base.String(), givenPath)
+	if !strings.HasPrefix(pathToFile, "/") {
+		path.WriteString("/")
+	}
+	path.WriteString(pathToFile)
+	return path.String()
 }
