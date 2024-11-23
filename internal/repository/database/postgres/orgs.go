c770d2b560b1b47f98c96deaed4188a0b84da437
Magomed Zakaryaev <zkryaev.dev@yandex.ru>
2024-11-23 18:54:03 +0300

    Added: Search org, Get org in fixed area

--- internal/repository/database/postgres/orgs.go
+++ internal/repository/database/postgres/orgs.go
@@ -10,8 +10,9 @@ import (
 )
 
 var (
-	ErrOrgExists   = errors.New("org already exists")
-	ErrOrgNotFound = errors.New("org not found")
+	ErrOrgExists    = errors.New("org already exists")
+	ErrOrgNotFound  = errors.New("org not found")
+	ErrOrgsNotFound = errors.New("orgs not found")
 )
 
 // Сохраняет в БД инфу об организации. Отдает org_id при успешном сохранении
@@ -70,7 +71,7 @@ func (p *PostgresRepo) OrgByEmail(ctx context.Context, email string) (*models.Or
 	}()
 
 	query := `
-		SELECT org_id, email, passwd_hash, name, type, city, address, telephone, lat, long, about FROM orgs
+		SELECT org_id, email, passwd_hash, name, rating, type, city, address, telephone, lat, long, about FROM orgs
         WHERE email = $1;
 	`
 	var org models.OrgRegister
@@ -98,7 +99,7 @@ func (p *PostgresRepo) OrgByID(ctx context.Context, id int) (*models.OrgRegister
 		}
 	}()
 	query := `
-		SELECT org_id, email, passwd_hash, name, type, city, address, telephone, lat, long, about FROM orgs
+		SELECT org_id, email, passwd_hash, name, rating, type, city, address, telephone, lat, long, about FROM orgs
         WHERE org_id = $1;
 	`
 	var org models.OrgRegister
@@ -112,4 +113,78 @@ func (p *PostgresRepo) OrgByID(ctx context.Context, id int) (*models.OrgRegister
 		return nil, fmt.Errorf("failed to commit tx: %w", err)
 	}
 	return &org, nil
+}
+
+/*
+	Получение организаций
+*/
+
+// Принимает две точки на карте по диагонале. Возвращает список организаций лежащий в диапазоне этих двух точек
+func (p *PostgresRepo) OrgsInArea(ctx context.Context, area *models.AreaParams) ([]*models.OrgSummary, error) {
+	tx, err := p.db.Beginx()
+	if err != nil {
+		return nil, fmt.Errorf("failed to start tx: %w", err)
+	}
+	defer func() {
+		if err != nil {
+			tx.Rollback()
+		}
+	}()
+	query := `SELECT org_id, name, rating, type FROM orgs
+		WHERE lat BETWEEN $1 AND $2
+		AND long BETWEEN $3 AND $4;
+		`
+	orgList := make([]*models.OrgSummary, 0, 1)
+	if err = tx.SelectContext(
+		ctx,
+		&orgList,
+		query,
+		area.Left.Lat,
+		area.Right.Lat,
+		area.Left.Long,
+		area.Right.Long,
+	); err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, ErrOrgsNotFound
+		}
+		return nil, fmt.Errorf("failed to get orgs at given area: %w", err)
+	}
+	if err = tx.Commit(); err != nil {
+		return nil, fmt.Errorf("failed to commit tx: %w", err)
+	}
+	return orgList, nil
+}
+
+// Принимает пагинацию, имя и тип организации. Возвращает соответствующие организации
+func (p *PostgresRepo) OrgsSearch(ctx context.Context, params *models.SearchParams) ([]*models.OrgInfo, error) {
+	tx, err := p.db.Beginx()
+	if err != nil {
+		return nil, fmt.Errorf("failed to start tx: %w", err)
+	}
+	defer func() {
+		if err != nil {
+			tx.Rollback()
+		}
+	}()
+	query := `SELECT org_id, name, rating, type, city, address, telephone, lat, long, about FROM orgs
+		WHERE ($1 = '' OR ILIKE('%' || $1 || '%'))
+		`
+	orgList := make([]*models.OrgInfo, 0, params.Limit)
+	if params.Type != "" {
+		query += ` AND type = $2 LIMIT $3 OFFSET $4;`
+		err = tx.SelectContext(ctx, &orgList, query, params.Name, params.Type, params.Limit, params.Offset)
+	} else {
+		query += ` LIMIT $2 OFFSET $3;`
+		err = tx.SelectContext(ctx, &orgList, query, params.Name, params.Limit, params.Offset)
+	}
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, ErrOrgsNotFound
+		}
+		return nil, fmt.Errorf("failed orgs searching: %w", err)
+	}
+	if err = tx.Commit(); err != nil {
+		return nil, fmt.Errorf("failed to commit tx: %w", err)
+	}
+	return orgList, nil
 }
