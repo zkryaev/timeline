34a6cca389cced6dba8ff04b0fc8c8d243f9eca4
Magomed Zakaryaev <zkryaev.dev@yandex.ru>
2024-11-24 15:35:26 +0300

    Added: org insertions for testing

---
+++ migrations/000002_insertion_orgs.up.sql
@@ -0,0 +1,12 @@
+INSERT INTO orgs (email, passwd_hash, name, type, city, address, telephone, lat, long, about, verified)
+VALUES
+('org1', 'hashed_1', 'Organization One', 'Парикмахерские', 'Moscow', '123 Main St, Moscow', '+7 123 456 7890', 55.7558, 37.6173,'', TRUE),
+('org2', 'hashed_2', 'Organization Two', 'Образование', 'Saint Petersburg', '456 Second St, Saint Petersburg', '+72345678901', 59.9343, 30.3351,'', TRUE),
+('org3', 'hashed_3', 'Organization Three', 'Медицина', 'Novosibirsk', '789 Third St, Novosibirsk', '+73456789012', 55.0084, 82.9357,'', TRUE),
+('org4', 'hashed_4', 'Organization Four', 'Спорт', 'Yekaterinburg', '321 Fourth St, Yekaterinburg', '+74567890123', 56.8389, 60.6057,'', TRUE),
+('org5', 'hashed_5', 'Organization Five', 'Развлечения', 'Nizhny Novgorod', '654 Fifth St, Nizhny Novgorod', '+75678901234', 56.2965, 43.9361,'', TRUE),
+('org6', 'hashed_6', 'Organization Six', 'Авто-сервис', 'Kazan', '987 Sixth St, Kazan', '+7 678 901 2345', 55.8304, 49.0661,'', TRUE),
+('org7', 'hashed_7', 'Organization Seven', 'Кафе и рестораны', 'Chelyabinsk', '135 Seventh St, Chelyabinsk', '+77890123456', 55.1644, 61.4368,'', TRUE),
+('org8', 'hashed_8', 'Organization Eight', 'Парикмахерские', 'Omsk', '246 Eighth St, Omsk', '+78901234567', 54.9920, 73.3686,'', TRUE),
+('org9', 'hashed_9', 'Organization Nine', 'Спорт', 'Rostov-on-Don', '357 Ninth St, Rostov-on-Don', '+79012345678', 47.2357, 39.7015,'', TRUE),
+('org10', 'hashed_10', 'Organization Ten', 'Развлечения', 'Ufa', '468 Tenth St, Ufa', '+70123456789', 54.7388, 55.9721,'', TRUE);
