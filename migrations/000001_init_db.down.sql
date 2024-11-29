87de943bb14619be498d9204b4c23bcd082671d9
Magomed Zakaryaev <zkryaev.dev@yandex.ru>
2024-11-29 17:57:44 +0300

    Reformed migration files

--- migrations/000001_init_db.down.sql
+++ migrations/000001_init_db.down.sql
@@ -1,8 +1,8 @@
+
 DROP TABLE IF EXISTS user_verify;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS orgs_verify;
+DROP TABLE IF EXISTS workers;
+DROPT TABLE IF EXISTS services;
+DROP TABLE IF EXISTS timetable;
 DROP TABLE IF EXISTS orgs;
-
-
-
-
--- migrations/000001_init_db.up.sql
+++ migrations/000001_init_db.up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS users(
+CREATE TABLE IF NOT EXISTS users (
     user_id SERIAL PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
     passwd_hash TEXT NOT NULL,
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS users(
     verified BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE IF NOT EXISTS user_verify(
+CREATE TABLE IF NOT EXISTS users_verify (
     user_verify_id SERIAL PRIMARY KEY,
     user_id INT,
     code VARCHAR(6) NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS user_verify(
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS orgs(
+CREATE TABLE IF NOT EXISTS orgs (
     org_id SERIAL PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
     passwd_hash TEXT NOT NULL,
@@ -36,7 +36,18 @@ CREATE TABLE IF NOT EXISTS orgs(
     verified BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE IF NOT EXISTS orgs_verify(
+CREATE TABLE IF NOT EXISTS timetables (
+    timetable_id SERIAL PRIMARY KEY,
+    org_id INT NOT NULL,
+    weekday INT NOT NULL,
+    open TIMESTAMP NOT NULL,
+    close TIMESTAMP NOT NULL,
+    break_start TIMESTAMP NOT NULL,
+    break_end TIMESTAMP NOT NULL,
+    FOREIGN KEY (org_id) REFERENCES orgs(org_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS orgs_verify (
     org_verify_id SERIAL PRIMARY KEY,
     org_id INT NOT NULL,
     code VARCHAR(6) NOT NULL,
@@ -44,5 +55,22 @@ CREATE TABLE IF NOT EXISTS orgs_verify(
     FOREIGN KEY (org_id) REFERENCES orgs(org_id) ON DELETE CASCADE
 );
 
-
-
+CREATE TABLE IF NOT EXISTS services (
+    service_id SERIAL PRIMARY KEY,
+    org_id INT,
+    name VARCHAR(300) NOT NULL,
+    cost NUMERIC(15,2) NOT NULL,
+    description VARCHAR(400),
+    FOREIGN KEY (org_id) REFERENCES orgs(org_id)
+);
+CREATE TABLE IF NOT EXISTS workers (
+    worker_id SERIAL PRIMARY KEY,
+    org_id INT,  -- Запятая добавлена
+    service_id INT,  -- Запятая добавлена
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255),
+    position VARCHAR(300),
+    degree VARCHAR(300),
+    FOREIGN KEY (service_id) REFERENCES services(service_id),
+    FOREIGN KEY (org_id) REFERENCES orgs(org_id)
+);
