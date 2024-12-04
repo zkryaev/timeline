ae4cf8d2a4aaed2efe3ab09f9c34b9cf64e8b785
Magomed Zakaryaev <zkryaev.dev@yandex.ru>
2024-12-04 18:09:56 +0300

    Separate models into domen's packages

--- internal/repository/models/services.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Service struct {
-	ServiceID   int     `db:"service_id"`
-	OrgID       int     `db:"org_id"`
-	Name        string  `db:"name"`
-	Cost        float64 `db:"cost"`
-	Description string  `db:"description"`
-}
