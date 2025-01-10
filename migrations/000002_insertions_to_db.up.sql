abc30d50f9c179d38fd7e1594de5155dc438e4fb
Magomed Zakaryaev <zkryaev.dev@yandex.ru>
2025-01-10 17:00:34 +0300

    updates

--- migrations/000002_insertions_to_db.up.sql
+++ migrations/000002_insertions_to_db.up.sql
@@ -3,9 +3,9 @@ INSERT INTO users (uuid, email, passwd_hash, first_name, last_name, telephone, c
 ('', 'elena.petrova@example.com', 'hashed_password2', 'Елена', 'Петрова', '+73431234568', 'Санкт-Петербург', 'Работаю в IT и увлекаюсь фотографией.', true),
 ('', 'sergey.sidorov@example.com', 'hashed_password3', 'Сергей', 'Сидоров', '+73431234569', 'Новосибирск', 'Инженер-электронщик с 10-летним опытом.', true);
 INSERT INTO orgs (uuid, email, passwd_hash, name, rating, type, city, address, telephone, lat, long, about, verified) VALUES
-('', 'clinic.moscow@example.com', 'hashed_password4', 'Клиника здоровья', 4.8, 'Медицинская клиника', 'Москва', 'ул. Ленина, д. 10', '+73431234570', 55.7558, 37.6173, 'Оказываем услуги диагностики и лечения.', true),
-('', 'gym.spb@example.com', 'hashed_password5', 'Фитнес-центр Спорт', 4.6, 'Фитнес-центр', 'Санкт-Петербург', 'пр. Невский, д. 20', '+73431234571', 59.9343, 30.3351, 'Современное оборудование и профессиональные тренеры.', true),
-('', 'library.nsk@example.com', 'hashed_password6', 'Библиотека знаний', 4.9, 'Библиотека', 'Новосибирск', 'ул. Красный проспект, д. 5', '+73431234572', 55.0084, 82.9357, 'Крупнейшая библиотека региона с богатым фондом.', true);
+('', 'clinic.moscow@example.com', 'hashed_password4', 'Клиника здоровья', 4.8, 'pharmacy', 'Москва', 'ул. Ленина, д. 10', '+73431234570', 55.7558, 37.6173, 'Оказываем услуги диагностики и лечения.', true),
+('', 'gym.spb@example.com', 'hashed_password5', 'Фитнес-центр Спорт', 4.6, 'gym', 'Санкт-Петербург', 'пр. Невский, д. 20', '+73431234571', 59.9343, 30.3351, 'Современное оборудование и профессиональные тренеры.', true),
+('', 'library.nsk@example.com', 'hashed_password6', 'Библиотека знаний', 4.9, 'education', 'Новосибирск', 'ул. Красный проспект, д. 5', '+73431234572', 55.0084, 82.9357, 'Крупнейшая библиотека региона с богатым фондом.', true);
 INSERT INTO timetables (org_id, weekday, open, close, break_start, break_end) VALUES
 (1, 1, '2024-11-28 08:00:00', '2024-11-28 20:00:00', '2024-11-28 12:00:00', '2024-11-28 13:00:00'),
 (1, 2, '2024-11-28 08:00:00', '2024-11-28 20:00:00', '2024-11-28 12:00:00', '2024-11-28 13:00:00'),
@@ -38,9 +38,9 @@ INSERT INTO worker_schedules (org_id, worker_id, weekday, start, over) VALUES
 (3, 3, 2, '2024-11-29 10:00:00', '2024-11-29 18:00:00'); -- Мария Васильева (Библиотекарь) на вторник
 
 INSERT INTO slots (worker_schedule_id, worker_id, date, session_begin, session_end, busy) VALUES
-(1, 1, CURRENT_DATE, CURRENT_DATE + TIME '08:00:00', CURRENT_DATE + TIME '09:00:00', false),
-(1, 1, CURRENT_DATE + INTERVAL '1 day', CURRENT_DATE + INTERVAL '1 day' + TIME '08:00:00', CURRENT_DATE + TIME '09:00:00', false),
-(1, 1, CURRENT_DATE, CURRENT_DATE + TIME '09:00:00', CURRENT_DATE + TIME '10:00:00', false),
+(1, 1, CURRENT_DATE, CURRENT_DATE + TIME '08:00:00', CURRENT_DATE + TIME '09:00:00', true),
+(1, 1, CURRENT_DATE + INTERVAL '1 day', CURRENT_DATE + INTERVAL '1 day' + TIME '08:00:00', CURRENT_DATE + TIME '09:00:00', true),
+(1, 1, CURRENT_DATE, CURRENT_DATE + TIME '09:00:00', CURRENT_DATE + TIME '10:00:00', true),
 (1, 1, CURRENT_DATE, CURRENT_DATE + TIME '10:00:00', CURRENT_DATE + TIME '11:00:00', false),
 (1, 1, CURRENT_DATE, CURRENT_DATE + TIME '11:00:00', CURRENT_DATE + TIME '12:00:00', false),
 (1, 1, CURRENT_DATE, CURRENT_DATE + TIME '13:00:00', CURRENT_DATE + TIME '14:00:00', false),
