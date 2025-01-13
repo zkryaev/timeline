99fb437854da08006abe570e857dd0fd100cfa44
Magomed Zakaryaev <zkryaev.dev@yandex.ru>
2025-01-13 18:30:30 +0300

    fixed issue with incorrect time compares

--- internal/libs/custom/time.go
+++ internal/libs/custom/time.go
@@ -7,15 +7,19 @@ import "time"
 // если t > u, то +1,
 // если t = u, то 0,
 func CompareTime(t time.Time, u time.Time) int {
-	switch {
-	// t = u
-	case t.Hour() == u.Hour(), t.Minute() == u.Minute():
-		return 0
-	// t > u
-	case (t.Hour() < u.Hour() && t.Minute() < u.Minute()) || (t.Hour() == u.Hour() && t.Minute() < u.Minute()):
+	tHour, tMinute := t.Hour(), t.Minute()
+	uHour, uMinute := u.Hour(), u.Minute()
+
+	if tHour < uHour {
 		return -1
-	// t < u
-	case (t.Hour() > u.Hour() && t.Minute() > u.Minute()) || (t.Hour() == u.Hour() && t.Minute() > u.Minute()):
+	}
+	if tHour > uHour {
+		return 1
+	}
+	if tMinute < uMinute {
+		return -1
+	}
+	if tMinute > uMinute {
 		return 1
 	}
 	return 0
