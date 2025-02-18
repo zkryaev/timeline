5c925292f9ff6aa98e6a950e102fe5a249a0b6f2
Magomed Zakaryaev <zkryaev.dev@yandex.ru>
2025-02-18 18:52:53 +0300

    pipeline sql queries test for feedbacks

---
+++ internal/infrastructure/database/postgres_test/feedback_test.go
@@ -0,0 +1,53 @@
+package postgres_test
+
+import (
+	"context"
+	"timeline/internal/entity/dto/recordto"
+	"timeline/internal/infrastructure/mapper/recordmap"
+)
+
+func (suite *PostgresTestSuite) TestFeedbackQueries() {
+	ctx := context.Background()
+
+	// (3, 5, 'Замечательная библиотека, много интересных книг!');
+	exp := &recordto.Feedback{
+		RecordID: 3, // хардкод!
+		Stars:    5,
+		Feedback: "Замечательная библиотека, много интересных книг!",
+	}
+
+	suite.NoError(suite.db.FeedbackSet(ctx, recordmap.FeedbackToModel(exp)))
+
+	params := &recordto.FeedbackParams{
+		Limit: 5,
+		Page: 1,
+	}
+
+	feedbkList, found, err := suite.db.FeedbackList(ctx, recordmap.FeedParamsToModel(params))
+	suite.NoError(err)
+	suite.NotZero(found)
+	suite.NotNil(feedbkList)
+
+	for _, feedbk := range feedbkList {
+		feedback := recordmap.FeedbackToDTO(feedbk)
+		if exp.RecordID == feedback.RecordID {
+			suite.Equal(exp, feedback)
+		}	
+	}
+	exp.Feedback = "ТЕСТИРОВАНИЕ"
+	suite.NoError(suite.db.FeedbackUpdate(ctx, recordmap.FeedbackToModel(exp)))
+
+	feedbkList, found, err = suite.db.FeedbackList(ctx, recordmap.FeedParamsToModel(params))
+	suite.NoError(err)
+	suite.NotZero(found)
+	suite.NotNil(feedbkList)
+
+	for _, feedbk := range feedbkList {
+		feedback := recordmap.FeedbackToDTO(feedbk)
+		if exp.RecordID == feedback.RecordID {
+			suite.Equal(exp, feedback)
+		}	
+	}
+	params.RecordID = exp.RecordID
+	suite.NoError(suite.db.FeedbackDelete(ctx, recordmap.FeedParamsToModel(params)))
+}
