41f12f114935b965a246f347535ec24912500d6c
Magomed Zakaryaev <zkryaev.dev@yandex.ru>
2025-02-19 19:47:36 +0300

    changed insertion

--- internal/infrastructure/database/postgres_test/feedback_test.go
+++ internal/infrastructure/database/postgres_test/feedback_test.go
@@ -9,18 +9,18 @@ import (
 func (suite *PostgresTestSuite) TestFeedbackQueries() {
 	ctx := context.Background()
 
-	// (3, 5, 'Замечательная библиотека, много интересных книг!');
+	// (2, 4, 'Хорошая тренировка, но хотелось бы больше внимания.');
 	exp := &recordto.Feedback{
-		RecordID: 3, // хардкод!
-		Stars:    5,
-		Feedback: "Замечательная библиотека, много интересных книг!",
+		RecordID: 2, // хардкод!
+		Stars:    4,
+		Feedback: "Хорошая тренировка, но хотелось бы больше внимания.",
 	}
 
 	suite.NoError(suite.db.FeedbackSet(ctx, recordmap.FeedbackToModel(exp)))
 
 	params := &recordto.FeedbackParams{
 		Limit: 5,
-		Page: 1,
+		Page:  1,
 	}
 
 	feedbkList, found, err := suite.db.FeedbackList(ctx, recordmap.FeedParamsToModel(params))
@@ -32,7 +32,7 @@ func (suite *PostgresTestSuite) TestFeedbackQueries() {
 		feedback := recordmap.FeedbackToDTO(feedbk)
 		if exp.RecordID == feedback.RecordID {
 			suite.Equal(exp, feedback)
-		}	
+		}
 	}
 	exp.Feedback = "ТЕСТИРОВАНИЕ"
 	suite.NoError(suite.db.FeedbackUpdate(ctx, recordmap.FeedbackToModel(exp)))
@@ -46,7 +46,7 @@ func (suite *PostgresTestSuite) TestFeedbackQueries() {
 		feedback := recordmap.FeedbackToDTO(feedbk)
 		if exp.RecordID == feedback.RecordID {
 			suite.Equal(exp, feedback)
-		}	
+		}
 	}
 	params.RecordID = exp.RecordID
 	suite.NoError(suite.db.FeedbackDelete(ctx, recordmap.FeedParamsToModel(params)))
