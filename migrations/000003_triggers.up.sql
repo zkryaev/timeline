9ab1bde9bd0a651276aa606051668b36468808b5
Magomed Zakaryaev <zkryaev.dev@yandex.ru>
2025-02-25 21:50:48 +0300

    fixed trigger for checking number of media files uploaded

--- migrations/000003_triggers.up.sql
+++ migrations/000003_triggers.up.sql
@@ -1,11 +1,28 @@
 CREATE OR REPLACE FUNCTION check_media_org_limit()
 RETURNS TRIGGER AS $$
 DECLARE
-    record_count INTEGER;
+    media_cnt INTEGER;
+    banner_cnt INTEGER;
 BEGIN
-    SELECT COUNT(*) INTO record_count FROM showcase WHERE org_id = NEW.org_id;
-    IF record_count >= 6 THEN
-        RAISE EXCEPTION 'The maximum number of records has been exceeded for org_id %', NEW.org_id;
+    IF NEW.org_id IS NULL THEN
+        RAISE EXCEPTION 'org_id cannot be NULL';  -- Или обработайте NULL как вам нужно
+    END IF;
+
+    SELECT
+        COUNT(org_id),
+        SUM(CASE WHEN type = 'banner' THEN 1 ELSE 0 END)
+    INTO
+        media_cnt,
+        banner_cnt
+    FROM showcase
+    WHERE org_id = NEW.org_id::INT;
+
+    IF NEW.type = 'banner' AND banner_cnt >= 1 THEN
+        RAISE EXCEPTION 'the banner already exists for org_id %', NEW.org_id;
+    END IF;
+
+    IF media_cnt > 5 THEN
+        RAISE EXCEPTION 'the image limit has been reached for org_id %', NEW.org_id;
     END IF;
     RETURN NEW;
 END;
