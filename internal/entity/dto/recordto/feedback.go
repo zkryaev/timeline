21286ac1fe8e6f8cd762b52b40ba00d581954789
zakaryaev.m <>
2025-03-02 13:19:51 +0300

    added fields: worker first/last name, service name, record date

--- internal/entity/dto/recordto/feedback.go
+++ internal/entity/dto/recordto/feedback.go
@@ -1,10 +1,14 @@
 package recordto
 
 type Feedback struct {
-	FeedbackID int    `json:"feedback_id,omitempty"`
-	RecordID   int    `json:"record_id,omitempty"`
-	Stars      int    `json:"stars,omitempty"`
+	FeedbackID int    `json:"feedback_id"`
+	RecordID   int    `json:"record_id"`
+	Stars      int    `json:"stars"`
 	Feedback   string `json:"feedback,omitempty"`
+	Service    string `json:"service_name"`
+	FirstName  string `json:"worker_first_name"`
+	LastName   string `json:"worker_last_name"`
+	RecordDate string `json:"record_date"`
 }
 
 type FeedbackList struct {
