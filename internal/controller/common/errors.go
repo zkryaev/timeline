7cccc6ece97bc6dd232c74066d3e80f1d9515d19
zkryaev <zkryaev.dev@yandex.ru>
2025-04-02 14:40:43 +0300

    expand errors

--- internal/controller/common/errors.go
+++ internal/controller/common/errors.go
@@ -1,7 +1,11 @@
 package common
 
+import "timeline/internal/usecase/common"
+
 // auth.go
 var (
 	ErrFailedLogin    = "invalid username or password"
 	ErrFailedRegister = "register failed"
+	ErrNotFound       = common.ErrNotFound
+	ErrNothingChanged = common.ErrNothingChanged
 )
