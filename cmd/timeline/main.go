c41543e7104657fd52b76d08d88c62e545c62ae1
zkryaev <zkryaev.dev@yandex.ru>
2025-04-05 22:10:03 +0300

    added LoadFromSrcData

--- cmd/timeline/main.go
+++ cmd/timeline/main.go
@@ -15,7 +15,8 @@ import (
 	"timeline/internal/infrastructure"
 	"timeline/internal/infrastructure/mail"
 	"timeline/internal/infrastructure/s3"
-	"timeline/internal/libs/cronjob"
+	"timeline/internal/sugar/cronjob"
+	"timeline/internal/utils/loader"
 	"timeline/pkg/logger"
 
 	"github.com/joho/godotenv"
@@ -37,34 +38,44 @@ func main() {
 	cfg := config.MustLoad()
 	successConnection := "Successfuly connected to"
 	// Инициализация логгера
-	logs := logger.New(cfg.App.Env)
-	logs.Info("application initializing...")
-	defer logs.Sync()
+	logger := logger.New(cfg.App.Env)
+	logger.Info("Application started")
+	defer logger.Sync()
 	db, err := infrastructure.GetDB(os.Getenv("DB"), cfg.DB)
 	if err != nil {
-		logs.Fatal("incorrect db type", zap.Error(err))
+		logger.Fatal("incorrect db type", zap.Error(err))
 	}
 	err = db.Open()
 	if err != nil {
-		logs.Fatal(
+		logger.Fatal(
 			fmt.Sprintf("failed to connect %s", os.Getenv("DB")),
 			zap.Error(err),
 		)
 	}
-	logs.Info(fmt.Sprintf("%s %s", successConnection, os.Getenv("DB")))
-	logs.Info(
-		"",
+	logger.Info(fmt.Sprintf("%s %s", successConnection, os.Getenv("DB")))
+	logger.Info(
+		"DB",
 		zap.String("database server", cfg.DB.Host+":"+cfg.DB.Port),
 		zap.String("ssl", cfg.DB.SSLmode),
 	)
 	defer db.Close()
 
+	if !cfg.App.IsBackDataLoaded {
+		logger.Info("Loading data from provided sources...")
+		if err := loader.LoadData(logger, db); err != nil {
+			logger.Fatal("failed", zap.Error(err))
+		}
+		logger.Info("Loading data is finished")
+	} else {
+		logger.Info("Skipped loading background data from sources")
+	}
+
 	// Поднимаем почтовый сервис параметрами по умолчанию
-	post := mail.New(cfg.Mail, logs, 0, 0, 0)
+	post := mail.New(cfg.Mail, logger, 0, 0, 0)
 	post.Start()
-	logs.Info(fmt.Sprintf("%s %s", successConnection, os.Getenv("MAIL_HOST")))
-	logs.Info(
-		"",
+	logger.Info(fmt.Sprintf("%s %s", successConnection, os.Getenv("MAIL_HOST")))
+	logger.Info(
+		"MAIL",
 		zap.String("mail server", cfg.Mail.Host+":"+strconv.Itoa(cfg.Mail.Port)),
 	)
 	defer post.Shutdown()
@@ -72,20 +83,20 @@ func main() {
 	// Подключение к S3
 	s3storage := s3.New(cfg.S3)
 	if err = s3storage.Connect(); err != nil {
-		logs.Fatal(fmt.Sprintf("failed to connect to %s", os.Getenv("S3")), zap.Error(err))
+		logger.Fatal(fmt.Sprintf("failed to connect to %s", os.Getenv("S3")), zap.Error(err))
 	}
-	logs.Info(fmt.Sprintf("%s %s", successConnection, os.Getenv("S3")))
-	logs.Info(
-		"",
+	logger.Info(fmt.Sprintf("%s %s", successConnection, os.Getenv("S3")))
+	logger.Info(
+		"S3",
 		zap.String("storage", cfg.S3.Host+":"+cfg.S3.DataPort),
 		zap.String("console", cfg.S3.Host+":"+cfg.S3.ConsolePort),
 		zap.Bool("ssl", cfg.S3.SSLmode),
 	)
 
-	app := app.New(cfg.App, logs)
+	app := app.New(cfg.App, logger)
 	err = app.SetupControllers(cfg.Token, db, post, s3storage)
 	if err != nil {
-		logs.Fatal(
+		logger.Fatal(
 			"failed setup controllers",
 			zap.Error(err),
 		)
@@ -102,27 +113,27 @@ func main() {
 		err = app.Run()
 		if err != nil {
 			if !errors.Is(err, http.ErrServerClosed) {
-				logs.Error("failed to run server", zap.Error(err))
+				logger.Error("failed to run server", zap.Error(err))
 				errorChan <- err
 			}
 		}
 	}()
-	logs.Info("application is running")
-	logs.Info("", zap.String("app server", cfg.App.Host+":"+cfg.App.Port))
+	logger.Info("application is running")
+	logger.Info("", zap.String("app server", cfg.App.Host+":"+cfg.App.Port))
 
 	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
 	select {
 	case sig := <-quit:
 		cancel()
-		logs.Info("Received signal",
+		logger.Info("Received signal",
 			zap.String("signal", sig.String()),
 		)
 	case err = <-errorChan:
 		cancel()
-		logs.Error("error occurred",
+		logger.Error("error occurred",
 			zap.Error(err),
 		)
 	}
 	app.Stop(ctx)
-	logs.Info("application stopped")
+	logger.Info("Application stopped")
 }
