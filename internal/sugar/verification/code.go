edc6204add163e63f5731103cbfbfb68e2a04679
zkryaev <zkryaev.dev@yandex.ru>
2025-04-09 17:33:41 +0300

    hotfix try

--- internal/sugar/verification/code.go
+++ internal/sugar/verification/code.go
@@ -6,16 +6,19 @@ import (
 	"strconv"
 )
 
-func GenerateCode() (string, error) {
-	minCode, maxCode := 1000, 9999 //nolint:revive // simple and fast
-	codesRange := minCode - maxCode + 1
+var (
+	minCode   int64 = 1000
+	maxCode   int64 = 9999
+	codeRange int64 = 1 + (maxCode - minCode)
+)
 
-	n, err := rand.Int(rand.Reader, big.NewInt(int64(codesRange)))
+func GenerateCode() (string, error) {
+	n, err := rand.Int(rand.Reader, big.NewInt(codeRange))
 	if err != nil {
 		return "", err
 	}
 
 	// Приводим результат к 4-значному числу и преобразуем его в строку
-	code := int(n.Int64()) + minCode
+	code := int(n.Int64() + minCode)
 	return strconv.Itoa(code), nil
 }
