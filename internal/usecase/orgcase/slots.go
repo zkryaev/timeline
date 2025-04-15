516a6360ac23cc1886ec38da4134cba52e2b9724
zkryaev <zkryaev.dev@yandex.ru>
2025-04-15 12:02:05 +0300

    removed update slot

--- internal/usecase/orgcase/slots.go
+++ internal/usecase/orgcase/slots.go
@@ -12,17 +12,6 @@ import (
 	"go.uber.org/zap"
 )
 
-func (o *OrgUseCase) UpdateSlot(ctx context.Context, logger *zap.Logger, req *orgdto.SlotUpdate) error {
-	if err := o.org.UpdateSlot(ctx, req.Busy, orgmap.SlotUpdateToModel(req)); err != nil {
-		if errors.Is(err, postgres.ErrNoRowsAffected) {
-			return common.ErrNothingChanged
-		}
-		return err
-	}
-	logger.Info("Slot has been updated")
-	return nil
-}
-
 func (o *OrgUseCase) Slots(ctx context.Context, logger *zap.Logger, req *orgdto.SlotReq) ([]*orgdto.SlotResp, error) {
 	data, city, err := o.org.Slots(ctx, orgmap.SlotReqToModel(req))
 	if err != nil {
