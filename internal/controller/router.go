93b50190046528fdcd3178e1fba5a277232af00a
zkryaev <zkryaev.dev@yandex.ru>
2025-04-16 18:14:39 +0300

    hotfix

--- internal/controller/router.go
+++ internal/controller/router.go
@@ -50,19 +50,17 @@ func InitRouter(controllersSet *Controllers, routes scope.Routes, settings *scop
 	authmuxProtected.HandleFunc(scope.PathCode, auth.CodeConfirm).Methods(routes[scope.PathCode].Methods.Get(scope.PUT)...)
 
 	// users
-	Protected.HandleFunc(scope.PathUsers, user.GetUser).Methods(routes[scope.PathUsers].Methods.Get(scope.GET)...)
-	Protected.HandleFunc(scope.PathUsers, user.UpdateUser).Methods(routes[scope.PathUsers].Methods.Get(scope.PUT)...)
-	// orgs
-	Protected.HandleFunc(scope.PathOrgs, org.GetOrganization).Methods(routes[scope.PathOrgs].Methods.Get(scope.GET)...)
-	Protected.HandleFunc(scope.PathOrgs, org.PutOrganization).Methods(routes[scope.PathOrgs].Methods.Get(scope.PUT)...)
-
 	usermuxProtected := Protected.NewRoute().PathPrefix(scope.PathUsers).Subrouter()
+	usermuxProtected.HandleFunc("", user.GetUser).Methods(routes[scope.PathUsers].Methods.Get(scope.GET)...)
+	usermuxProtected.HandleFunc(scope.PathUsers, user.UpdateUser).Methods(routes[scope.PathUsers].Methods.Get(scope.PUT)...)
 	// users/orgmap
 	usermuxProtected.HandleFunc(scope.PathMapOrgs, user.OrganizationInArea).Methods(routes[scope.PathMapOrgs].Methods.Get(scope.GET)...)
 	// users/search/org
 	usermuxProtected.HandleFunc(scope.PathSearchOrgs, user.SearchOrganization).Methods(routes[scope.PathSearchOrgs].Methods.Get(scope.GET)...)
-
+	// orgs
 	orgmuxProtected := Protected.NewRoute().PathPrefix(scope.PathOrgs).Subrouter()
+	orgmuxProtected.HandleFunc(scope.PathOrgs, org.GetOrganization).Methods(routes[scope.PathOrgs].Methods.Get(scope.GET)...)
+	orgmuxProtected.HandleFunc(scope.PathOrgs, org.PutOrganization).Methods(routes[scope.PathOrgs].Methods.Get(scope.PUT)...)
 	// orgs/timetables
 	orgmuxProtected.HandleFunc(scope.PathTimetables, org.TimetableAdd).Methods(routes[scope.PathTimetables].Methods.Get(scope.POST)...)
 	orgmuxProtected.HandleFunc(scope.PathTimetables, org.Timetable).Methods(routes[scope.PathTimetables].Methods.Get(scope.GET)...)
